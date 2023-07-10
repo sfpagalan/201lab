5d758c58c99a4d8be5cabf3d2b61fe4f149b3773
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-07-10 15:43:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function name() {
+    let name = prompt('What is your name?'); 
+    alert('Hello '+ name);
+    console.log('Hi there + name');
+}
+
+function home() {
+    let home = prompt('Where are you from?');
+    alert('Oh, so you are from '+ home);
+    console.log('You are from '+ home);
+}
+
+function anime() {
+    let anime = prompt('What is your favorite or at least an anime you like?');
+    alert(anime + ' is a good one!');
+    console.log(anime + 'is a good one!');
+} 
+
+function sidekick() {
+    let sidekick = prompt('You gotta pick a sidekick.');
+    alert(sidekick + ' as a companion is a good choice!');
+    console.log(sidekick + ' is a good companion!');
+}
